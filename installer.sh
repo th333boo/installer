763ef41a1b1dfaf25875a29fc127ab4154abab36
th333boo <th333boo@bbiwebsec.com>
2023-05-18 11:45:29 +0200

    add bashrc + dns resolver

--- installer.sh
+++ installer.sh
@@ -19,6 +19,41 @@ $HEADER" > '/etc/motd'
 cat /etc/motd
 sleep 2
 
+
+printf '\033[32m\n### [ CONFIG TIMEZONE ] ###\033[0m\n'
+echo "==============================="
+timedatectl set-timezone Europe/Paris
+sleep 2
+
+printf '\033[32m\n### [ BASHRC SHORTCUT ]###\033[0m\n'
+echo "==============================="
+echo "$HEADER
+
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias diff='diff --color=auto'
+alias ip='ip --color=auto'
+alias la='ls -A'
+alias l='ls -CF'
+alias ll='ls -la'
+alias lll='ls -lastr'
+alias cc='clear'
+alias oss='watch -n0.5 netstat -lapute'
+alias bbifw='watch -n0.5 iptables -L -nv --line-numbers'
+alias bbinet='tcpdump -i any'
+alias nn='cd /etc/nginx/conf.d/'
+alias ww='cd /var/www/'
+alias bbilog='tail -f /var/log/*log /var/log/*/*log'
+alias bbinstaller='/usr/bin/bash -c '/opt/installer''
+alias bbiupdate='touch /opt/installer && chmod 700 /opt/installer && > /opt/installer && nano /opt/installer && /usr/bin/bash -c '/opt/installer''
+alias bbibashrc='cd /root/ &&  > /root/.bashrc &&  nano /root/.bashrc && source /root/.bashrc'
+alias bbiss='sudo su'
+
+$HEADER" > '/root/.bashrc'
+source /root/.bashrc
+source /root/.bashrc_aliases
+
 printf '\033[32m\n### [ NETWORK CARD SETUP ]###\033[0m\n'
 echo "==============================="
 
@@ -63,6 +98,27 @@ DNSOverTLS=yes
 #ResolveUnicastSingleLabel=no
 
 $HEADER" > '/etc/systemd/resolved.conf'
+
+printf '\033[32m\n### [ CONFIG DEPOT APT ] ###\033[0m\n'
+echo "==============================="
+rm /etc/apt/sources.list /etc/apt/sources.list.bk
+echo "$HEADER
+
+### [ DEBIAN 11 BULLEYE, SECURITY UPDATE ] ###
+deb [signed-by=/etc/apt/trusted.gpg.d/debian-archive-bullseye-stable.gpg] https://deb.debian.org/debian bullseye main
+deb-src [signed-by=/etc/apt/trusted.gpg.d/debian-archive-bullseye-stable.gpg] https://deb.debian.org/debian bullseye main
+deb [signed-by=/etc/apt/trusted.gpg.d/debian-archive-bullseye-security-automatic.gpg] https://deb.debian.org/debian-security/ bullseye-security main
+deb-src [signed-by=/etc/apt/trusted.gpg.d/debian-archive-bullseye-security-automatic.gpg] https://deb.debian.org/debian-security/ bullseye-security main
+deb [signed-by=/etc/apt/trusted.gpg.d/debian-archive-bullseye-stable.gpg] https://deb.debian.org/debian bullseye-updates main
+deb-src [signed-by=/etc/apt/trusted.gpg.d/debian-archive-bullseye-stable.gpg] https://deb.debian.org/debian bullseye-updates main
+deb [signed-by=/etc/apt/trusted.gpg.d/debian-archive-bullseye-security-automatic.gpg] https://security.debian.org/debian-security bullseye-security main
+deb-src [signed-by=/etc/apt/trusted.gpg.d/debian-archive-bullseye-security-automatic.gpg] https://security.debian.org/debian-security bullseye-security main
+
+$HEADER" > '/etc/apt/sources.list.d/debian.list'
+
+printf '\033[32m\n### [ UPDATE && INSTALL PACKAGES ]###\033[0m\n'
+echo "==============================="
+apt update
 
 printf '\033[32m\nUPDATE INSTALLER  \033[0m\n'
 echo "==============================="
